dad27749105eb6178b926cc00b8f0366c8dcb0bb
kimber <kimber.gee@gmail.com>
2016-06-15 16:29:46 -0400

    user can sign in and out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
+
+  private
+
+  def require_sign_in
+    unless current_user
+      flash[:error] = "You must be logged in to do that"
+      redirect_to new_user_session_path
+    end
+  end
 end
